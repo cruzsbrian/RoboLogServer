3bd28ba749f1af3a9181cc6cb20f923792928864
cruzsbrian <cruz.s.brian@gmail.com>
2017-10-07 12:22:15 -0400

    Receiving constants and beginning to decode

--- src/Constants.java
+++ src/Constants.java
@@ -1,18 +1,23 @@
-import java.util.HashMap;
-import java.util.List;
 
 public class Constants {
-	
+
 	public static Object get(String key) {
 		return null;
 	}
-	
+
 	public static void add(String key, Object val) {
-		
+
 	}
-	
-	public static void addAll(List<HashMap<String, Object>> list) {
-		
+
+	public static void addAll(Constant[] list) {
+		for (int i = 0; i < list.length; i++) {
+			System.out.println(list[i].toString());
+		}
+	}
+
+	protected class Constant {
+		public String key;
+		public String val;
 	}
 
 }
--- src/Handler.java
+++ src/Handler.java
@@ -1,8 +1,3 @@
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketListener;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
@@ -10,71 +5,75 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Handler implements WebSocketListener {
-	
+
 	static Session session;
 	static boolean connected = false;
-	
-	private static Gson gson = new Gson();
-	
+
 	@OnWebSocketClose
 	public void onWebSocketClose(int statusCode, String reason) {
 		Log.printRoboLog();
 		System.out.println("Connection closed");
-		
+
 		connected = false;
 	}
-	
+
 	@OnWebSocketError
 	public void onWebSocketError(Throwable t) {
 		Log.printRoboLog();
 		System.out.println("Error: " + t.getMessage());
 	}
-	
+
 	@OnWebSocketConnect
 	public void onWebSocketConnect(Session s) {
 		Log.printRoboLog();
 		System.out.println("Connection opened with client");
-		
+
 		session = s;
 		connected = true;
 	}
-	
+
 	@OnWebSocketMessage
-	public void onWebSocketMessage(String msg) {
-		HashMap<String, Object> data = gson.fromJson(msg, HashMap.class);
-		if (data.get("type") == "constants") {
-			try {
-				List<HashMap<String, Object>> constants = (List<HashMap<String, Object>>) data.get("obj");
-				Constants.addAll(constants);
-			} catch (Exception e) {
-				Log.printRoboLog();
-				System.out.println("Error parsing constants from client");
-				e.printStackTrace();
-			}
-		}
-	}
-	
+	public void onWebSocketMessage(String msg) {}
+
 	public static void push(String msg) throws Exception {
 		session.getRemote().sendString(msg);
 	}
-	
+
 	public static boolean isConnected() {
 		return connected;
 	}
 
 	@Override
-	public void onWebSocketBinary(byte[] arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onWebSocketBinary(byte[] arg0, int arg1, int arg2) {}
 
 	@Override
-	public void onWebSocketText(String arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onWebSocketText(String msg) {
+		JsonParser parser = new JsonParser();
+
+		JsonElement root = parser.parse(msg);
+		if (root.isJsonObject()) {
+			JsonObject data = root.getAsJsonObject();
+
+			String dataType = data.get("type").getAsString();
+
+			Log.printRoboLog();
+			System.out.println("Received " + dataType + " from client");
+
+			if (dataType.equals("constants")) {
+				JsonArray constants = data.getAsJsonArray("obj");
+				System.out.println(constants.toString());
+			}
+		} else {
+			Log.printRoboLog();
+			System.out.println("Error parsing JSON from client:");
+			System.out.println(msg);
+		}
 	}
 
 }
