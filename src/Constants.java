fa848c3b872ef974f23cf78e0d40e852fcdb42b6
Brian Cruz <cruz.s.brian@gmail.com>
2017-10-07 11:32:52 -0400

    Receiving constants (not yet tested)

---
+++ src/Constants.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class Constants {
+	
+	public static Object get(String key) {
+		return null;
+	}
+	
+	public static void add(String key, Object val) {
+		
+	}
+	
+	public static void addAll(List<HashMap<String, Object>> list) {
+		
+	}
+
+}
